ee34ab9f18a118360ac50c9851a96391d586f1d6
Yasser <yacasia@gmail.com>
2021-10-11 10:16:47 -0500

    Fetch and display Topic details

--- src/scenes/TopicDetails.jsx
+++ src/scenes/TopicDetails.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import LoadingOrErrorCatcher from "../components/LoadingOrErrorCatcher";
+import { getTopicByName } from "../apis/github/graphql";
+import useGraphQLAPI from "../hooks/useGraphQLAPI";
 
 function TopicDetails({ match }) {
   const topicName = match?.params?.topicName;
+
+  const [data, isLoading, errorMsg, fetchData] = useGraphQLAPI(getTopicByName);
+
+  useEffect(() => {
+    fetchData(topicName);
+  }, [topicName, fetchData]);
+
+  const relatedTopics = data?.topic?.relatedTopics;
+  const stargazerCount = data?.topic?.stargazerCount;
 
   return (
     <div>
       <div className="mb-3">
         <Link to="/">&lt; Go topic search</Link>
       </div>
-      <h2>{topicName}</h2>
+      <LoadingOrErrorCatcher isLoading={isLoading} errorMsg={errorMsg}>
+        <h2># {topicName}</h2>
+        <p>Stargazer count: {stargazerCount}</p>
+        <p>
+          {relatedTopics?.length && "Related to: "}
+          {relatedTopics?.map(({ name }) => (
+            <span className="px-2">
+              <Link to={`/${name}`}>{name}</Link>
+            </span>
+          ))}
+        </p>
+      </LoadingOrErrorCatcher>
     </div>
   );
 }
